4bf1b5b39c7b14a54238085155f208f16daf4344
pal-codes <ap.aayushpal@gmail.com>
2019-09-16 20:51:13 +0530

    layouting app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Mainer from './components/Mainer';
 import Avatar from './components/Avatar';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Avatar />
-      <Mainer />
+      <Container>
+      <Row>
+        <Col xs={12} xl lg={12}>
+          <Avatar />
+        </Col>
+      </Row>
+      <Row>
+        <Mainer />
+      </Row>
+      </Container>
     </div>
   );
 }
--- src/components/Mainer.js
+++ src/components/Mainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Name = styled.h1`
+    font-size: 5rem;
+`;
+
 
 export default function Mainer () {
     return (
         <Container>
-            <h1>Aayush Pal</h1>
+            <Name>Aayush Pal</Name>
             <h3>Brainstormer, Engineer</h3>
         </Container>
     )
